26b98ab82424fe023e16c5232d5f1ebcbd4e261a
Anthony Grear <tgrear2008@gmail.com>
2021-11-28 14:57:44 +0700

    Add user choice of states quiz or capitals quiz.

--- StateCapitalQuiz/src/states/quiz/Quiz.java
+++ StateCapitalQuiz/src/states/quiz/Quiz.java
@@ -11,19 +11,49 @@ public class Quiz {
 		StatesAndCaps st = new StatesAndCaps(); 
 		arr = st.importList();
 		int count=0;
+		int correct=0;
 		String userResponse;
+		int choice1 = 0;
+		int choice2 = 1;
+		String question = null;
 		
+		System.out.println("Welcome to the US States and Capitols Quiz\n\n"
+				+ "For the state capitals quiz, enter 'caps'."
+				+ "\nFor the states quiz, enter 'state'.\nEnter 'quit' to exit.");
 		Scanner sc = new Scanner(System.in);
-		do {
-			System.out.println("What is the capital city of " + arr[count][1]+"?");
+		userResponse = sc.nextLine().toLowerCase();
+		
+		if (userResponse.equals("caps")) {
+			choice1 = 1;
+			choice2 = 0;
+			question = "What is the capital city of the following US state? ";
+		} else if (userResponse.equals("states") ) {
+			choice1 = 0;
+			choice2 = 1;
+			question = "To what US state does the following capital city belong? ";
+		} else {
+			count = 50;
+		}
+		
+		while (count!=50) {
+			System.out.println(question + arr[count][choice1]);
 			userResponse = sc.nextLine();
 			userResponse = userResponse.toLowerCase();
-			if (arr[count][0].toLowerCase().equals(userResponse) ) {
-				System.out.println("You are correct");
+			String correctString = String.valueOf(correct);
+			if (userResponse.equals("quit")) {
+				System.out.println("Thank you for playing.\nYour total score: " + correctString);
+				break;
+			}
+			if (arr[count][choice2].toLowerCase().equals(userResponse) ) {
+				correct++;
+				correctString = String.valueOf(correct);  
+				System.out.println("You are correct.\nTotal Score: "+ correctString);
+				
+				
 			} else {
 				System.out.println("You are incorrect");
 			}
 			count++;
-		} while (count!=50);
+		} 
 	}
 }
